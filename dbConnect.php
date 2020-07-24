85aa2700f681ef61428253a332148cbc86ce1940
IlhamShiddiq <shdqillham123@gmail.com>
2020-07-24 08:37:16 +0700

    hosting

--- dbConnect.php
+++ dbConnect.php
@@ -6,4 +6,13 @@
     } catch(PDOException $e){
         echo $e->getMessage();
     }
+
+    // Hosting
+    // try {
+    //     $con = new PDO("mysql:host=localhost;dbname=id14449555_corona_web_db", "id14449555_corona", "}uQoCyu6?_)bXgH3");
+    //     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // } catch(PDOException $e){
+    //     echo $e->getMessage();
+    // }
+
 ?>
